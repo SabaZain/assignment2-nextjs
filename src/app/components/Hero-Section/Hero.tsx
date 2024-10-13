b4883aea800cecaa41d3b05f2dbbcef26e15d01b
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-10-13 02:34:53 -0700

    Update Hero.tsx

--- src/app/components/Hero-Section/Hero.tsx
+++ src/app/components/Hero-Section/Hero.tsx
@@ -4,7 +4,7 @@ import styles from "./hero.module.css";
 export default function Hero () {
   return (
     <section className={styles.hero}>
-        <h2 className={styles.main}>Every step tells a story.<br/><br/> Explore our stylish and comfortable ladies' shoes that empower you to express yourself.<br/><br/>
+        <h2 className={styles.main}>Every step tells a story.<br/><br/> Explore our stylish and comfortable ladies shoes that empower you to express yourself.<br/><br/>
             Join us in celebrating every moment, one step at a time.</h2>
         <br/>
         <Link href="/Latest">

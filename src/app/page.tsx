5972a96d372b7fe9de32a47c94af91cbc18a9a39
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-10-13 02:38:33 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero-Section/Hero";

c92d8c0d445b1d1c2c6922b69d18cc1c7e572f1d
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-10-13 02:27:51 -0700

    Update page.tsx

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -8,7 +8,7 @@ const AboutPage = () => {
     <div>
       <Header/>
       <h1 className={styles.heading}>About Us</h1>
-      <p className={styles.paragraph}>Welcome to <span className={styles.name}>S@Z EazyWe@r</span>, stylish and comfortable ladies' shoes!<br/> Our passion for footwear drives us to curate a collection that blends fashion, quality, and affordability.
+      <p className={styles.paragraph}>Welcome to <span className={styles.name}>S@Z EazyWe@r</span>, stylish and comfortable ladies shoes!<br/> Our passion for footwear drives us to curate a collection that blends fashion, quality, and affordability.
          we believe that every woman deserves to feel confident and beautiful in her shoes, whether she is heading to work, enjoying a night out, or relaxing at home. 
          We carefully select each pair to ensure they meet our high standards of comfort and style.
          We are committed to providing exceptional customer service, making your shopping experience as delightful as possible. Our team is here to help you find the perfect pair.</p>

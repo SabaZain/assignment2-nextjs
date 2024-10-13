d8128d747ad51bad77984a9df0162ed3a6eafda2
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-10-13 04:13:28 -0700

    Update page.tsx

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Header from "../components/Header/Header";
 import Image from "next/image";
 import styles from "./home.module.css";
-import Footer from "../components/Footer/Footer";
+
 
 const HomePage = () => {
   return (
